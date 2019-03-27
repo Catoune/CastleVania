7d22d7fa79092e30313d01ac0a47475ec4e8c716
Luana-GD <l.trannguyen@rubika-edu.com>
2019-03-27 17:37:49 +0100

    Ecran titre fait. Problème de merge sur scene_01, y'a plus rien. Ecran de pause fait mais le script est incomplet. L'UI est incomplète.

---
+++ Assets/LTN_UIstuff/LTNscripts/SwitchScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SwitchScene : MonoBehaviour
+{
+
+
+    public void LoadSceneTitle()
+    {
+            SceneManager.LoadScene("Scene_01", LoadSceneMode.Single);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
