18f514ebed172b03a396fe61c2a1e22b046dcf71
LUKAS Margaux <m.lukas@rubika-edu.com>
2019-04-09 16:51:12 +0200

    Breakable Platform Respawn

--- Assets/_Scripts/FallingPlatform.cs
+++ Assets/_Scripts/FallingPlatform.cs
@@ -5,23 +5,58 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
+    private Animator animator;
+
+    public GameObject fallingPlatform;
+    public Transform generationPoint;
+    public static float breakTimer;
+    public static bool breakTimerStart = false;
+    public static float spawnTimer;
+    public static bool spawnTimerStart = false;
+    public static bool isPlatformDestroyed = false;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        generationPoint = fallingPlatform.GetComponent<Transform>();
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            transform.position -= Vector2(0,-1) * 
+            breakTimerStart = true;
+            animator.SetBool("TimerStart", true);
+
+            if (breakTimer == 3)
+            {
+                Destroy(this);
+                isPlatformDestroyed = true;
+            }
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isPlatformDestroyed == true)
+        {
+            spawnTimerStart = true;
+        }
+
+        if (breakTimerStart == true)
+        {
+            breakTimer += Time.deltaTime;
+        }
+
+        if (spawnTimerStart == true)
+        {
+            spawnTimer += Time.deltaTime;
+        }
+
+        if (spawnTimer == 10)
+        {
+            Instantiate(fallingPlatform, generationPoint.position, generationPoint.rotation);
+        }
     }
 }
