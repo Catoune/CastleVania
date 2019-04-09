425d5493a32811a59c1abacd1af87ac864de8a4b
LUKAS Margaux <m.lukas@rubika-edu.com>
2019-04-09 13:31:17 +0200

    OneWay Platform + Breakable Platform WIP

---
+++ Assets/_Scripts/FallingPlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            transform.position -= Vector2(0,-1) * 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
