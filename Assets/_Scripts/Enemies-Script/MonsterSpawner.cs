9657d6801180d7ae6a1ffe248a9b82afaab94c28
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-04-10 22:06:56 +0200

    Version 2.3 - Monster Spawner

--- Assets/_Scripts/Enemies-Script/MonsterSpawner.cs
+++ Assets/_Scripts/Enemies-Script/MonsterSpawner.cs
@@ -1,53 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MonsterSpawner : MonoBehaviour {
 
-	// all public var need to be init in Inspector
+//Attaché à un trigger
+public class MonsterSpawner : MonoBehaviour
+{
 	public GameObject  demonPrefab;
-	public int numberOfMonsters;
 	public Vector2 createAt;
-	private int demonNum = 0;
+    public int numberOfMonsters;
+    private int demonNum = 0;
 	public float creationDelayInSec = 0.5f;
 	public float refreshTime = 5.0f;
-	// Use this for initialization
-	void Start () {
-	}
 	
-	void OnTriggerEnter2D(Collider2D other){
-		if (other.gameObject.tag == "Player") {
-			StartCoroutine(createMoster());
-		}
+	void OnTriggerEnter2D(Collider2D other)
+    {
+		if (other.gameObject.tag == "Player") {StartCoroutine(spawnMonster());}
 	}
 
-	IEnumerator createMoster()
+	IEnumerator spawnMonster()
 	{
-		while (GetComponent<Collider2D>().enabled == true) {
-			for (; demonNum < numberOfMonsters; ++demonNum) {
+		while (GetComponent<Collider2D>().enabled == true)
+        {
+			for (; demonNum < numberOfMonsters; ++demonNum)
+            {
 				GameObject gameObj = Instantiate (demonPrefab) as GameObject;
-				gameObj.transform.position = 
-					new Vector3 (transform.position.x + createAt.x, 
-            	transform.position.y + createAt.y, 0);
+				gameObj.transform.position = new Vector3 (transform.position.x + createAt.x, transform.position.y + createAt.y, 0);
 				yield return new WaitForSeconds (creationDelayInSec);
-				if(GetComponent<Collider2D>().enabled == false)
-					yield return true;
+                if (GetComponent<Collider2D>().enabled == false) { yield return true; }
 			}
 			yield return new WaitForSeconds (refreshTime/2f);
-			if(GetComponent<Collider2D>().enabled == false)
-				yield return true;
+            if (GetComponent<Collider2D>().enabled == false) { yield return true; }
 			yield return new WaitForSeconds (refreshTime/2f);
-
 			demonNum = 0;
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D other)
 	{
-		if (other.gameObject.tag == "Player") {
-			Debug.Log ("Disabled collider");
+		if (other.gameObject.tag == "Player")
+        {
 			StartCoroutine(waitToReset());
-
-
 		}
 	}
 
@@ -56,6 +48,5 @@ public class MonsterSpawner : MonoBehaviour {
 		GetComponent<Collider2D>().enabled = false;
 		yield return new WaitForSeconds (refreshTime);
 		GetComponent<Collider2D>().enabled = true;
-
 	}
 }
