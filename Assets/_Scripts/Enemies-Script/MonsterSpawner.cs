ebeefeb72ea3c8d448564fa7a6b40dfdadc2a687
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-04-10 22:25:27 +0200

    Clean Up - Music

--- Assets/_Scripts/Enemies-Script/MonsterSpawner.cs
+++ Assets/_Scripts/Enemies-Script/MonsterSpawner.cs
@@ -5,12 +5,14 @@ using System.Collections;
 //Attaché à un trigger
 public class MonsterSpawner : MonoBehaviour
 {
-	public GameObject  demonPrefab;
-	public Vector2 createAt;
-    public int numberOfMonsters;
-    private int demonNum = 0;
-	public float creationDelayInSec = 0.5f;
-	public float refreshTime = 5.0f;
+	public GameObject monsterPrefab     ; // Monstre à faire spawn
+	public Vector2    createAt          ; // Position de création
+
+    public  int numberMonsters          ; // Nombre de monstre à faire Spawn
+    private int currentMonsterNumber = 0; // Nombre actuel de monstre
+
+	public float creationDelay = 0.5f   ; // Temps avant de spawn un autre monstre
+	public float refreshTime   = 5.0f   ; // Temps avant de repartir à 0
 	
 	void OnTriggerEnter2D(Collider2D other)
     {
@@ -21,17 +23,17 @@ public class MonsterSpawner : MonoBehaviour
 	{
 		while (GetComponent<Collider2D>().enabled == true)
         {
-			for (; demonNum < numberOfMonsters; ++demonNum)
+			for (; currentMonsterNumber < numberMonsters; ++currentMonsterNumber)
             {
-				GameObject gameObj = Instantiate (demonPrefab) as GameObject;
+				GameObject gameObj = Instantiate (monsterPrefab) as GameObject;
 				gameObj.transform.position = new Vector3 (transform.position.x + createAt.x, transform.position.y + createAt.y, 0);
-				yield return new WaitForSeconds (creationDelayInSec);
+				yield return new WaitForSeconds (creationDelay);
                 if (GetComponent<Collider2D>().enabled == false) { yield return true; }
 			}
 			yield return new WaitForSeconds (refreshTime/2f);
             if (GetComponent<Collider2D>().enabled == false) { yield return true; }
 			yield return new WaitForSeconds (refreshTime/2f);
-			demonNum = 0;
+			currentMonsterNumber = 0;
 		}
 	}
 
