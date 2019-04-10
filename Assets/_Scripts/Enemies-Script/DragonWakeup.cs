636b92b379ad0a38c72c74225e36da65fd9d8155
Alexandre Courtin <aoyama_masaya@hotmail.fr>
2019-04-10 21:57:28 +0200

    Clean Up 1.3 - Debug

--- Assets/_Scripts/Enemies-Script/DragonWakeup.cs
+++ Assets/_Scripts/Enemies-Script/DragonWakeup.cs
@@ -1,33 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DragonWakeup : MonoBehaviour {
-
-
+public class DragonWakeup : MonoBehaviour
+{
 	public float randomIndex;
 	public bool sprial;
 	public float Rate; // shoot interval
 	public float AngleIncrement = 20.0f;
-	void OnTriggerEnter2D(Collider2D other){
-		if (other.gameObject.tag == "Player") {
-			WakeUp ();
-		}
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+		if (other.gameObject.tag == "Player") {WakeUp ();}
 	}
 
-
-	public virtual void WakeUp (){
-
+	public virtual void WakeUp ()
+    {
 		ShootFireBall[] lmScript = GetComponentsInChildren<ShootFireBall>();
 		
-		
-		
-		foreach (ShootFireBall sfb in lmScript) {
+		foreach (ShootFireBall sfb in lmScript)
+        {
 			float wakeupTime = Random.Range(0.0f, randomIndex);
 			sfb.wakeUp (wakeupTime, sprial, Rate, AngleIncrement);
 			
 		}
-		
 		GetComponent<Collider2D>().enabled = false;
 	}
-
 }
